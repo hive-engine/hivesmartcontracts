c35b5fa06d54f9bd24791e12e9bc5f57ee6b3f69
eonwarped <eonwarped@gmail.com>
2025-01-08 05:26:39 +0100

    do not expose bignumber reference object, remove other ivm references after setting up the wrappers

--- libs/SmartContracts.js
+++ libs/SmartContracts.js
@@ -567,12 +567,9 @@ class SmartContracts {
           resolve(error);
         });
         vm.context.global.setSync('sscglobal_externalCopy', x => new ivm.ExternalCopy(x));
-        Object.keys(vmState.api).forEach((key) => {
-            vm.context.global.setSync('sscglobal_' + key, key === 'payload' && typeof(vmState.api[key]) === 'object' ? new ivm.ExternalCopy(vmState.api[key]) : vmState.api[key]);
-            });
-        Object.keys(vmState.db).forEach((key) => {
-            vm.context.global.setSync('sscglobal_' + key, vmState.db[key]);
-            });
+        vm.context.global.setSync('sscglobal_api', new ivm.Reference(vmState.api));
+        vm.context.global.setSync('sscglobal_debug', vmState.api.debug);
+        vm.context.global.setSync('sscglobal_db', new ivm.Reference(vmState.db));
         vm.context.global.setSync('sscglobalv_isAlpha', new ivm.Callback(validator.isAlpha));
         vm.context.global.setSync('sscglobalv_isAlphanumeric', new ivm.Callback(validator.isAlphanumeric));
         vm.context.global.setSync('sscglobalv_blacklist', new ivm.Callback(validator.blacklist));
@@ -609,6 +606,38 @@ class SmartContracts {
         vm.context.global.setSync('sscglobal_bn_max', new ivm.Reference((...args) => BigNumber.max.apply(undefined, args.map(maybeDeref))));
 
         const wrappedCode = 'new ' + function() {
+          let _sscglobal_api = sscglobal_api;
+          let _sscglobal_debug = sscglobal_debug;
+          let _sscglobal_db = sscglobal_db;
+          let _sscglobal_externalCopy = sscglobal_externalCopy;
+          let _sscglobal_bn_construct = sscglobal_bn_construct;
+          let _sscglobal_bn_plus = sscglobal_bn_plus;
+          let _sscglobal_bn_minus = sscglobal_bn_minus;
+          let _sscglobal_bn_times = sscglobal_bn_times;
+          let _sscglobal_bn_multipliedBy = sscglobal_bn_multipliedBy;
+          let _sscglobal_bn_dividedBy = sscglobal_bn_dividedBy;
+          let _sscglobal_bn_sqrt = sscglobal_bn_sqrt;
+          let _sscglobal_bn_pow = sscglobal_bn_pow;
+          let _sscglobal_bn_negated = sscglobal_bn_negated;
+          let _sscglobal_bn_abs = sscglobal_bn_abs;
+          let _sscglobal_bn_lt = sscglobal_bn_lt;
+          let _sscglobal_bn_lte = sscglobal_bn_lte;
+          let _sscglobal_bn_eq = sscglobal_bn_eq;
+          let _sscglobal_bn_gt = sscglobal_bn_gt;
+          let _sscglobal_bn_gte = sscglobal_bn_gte;
+          let _sscglobal_bn_dp = sscglobal_bn_dp;
+          let _sscglobal_bn_decimalPlaces = sscglobal_bn_decimalPlaces;
+          let _sscglobal_bn_isNaN = sscglobal_bn_isNaN;
+          let _sscglobal_bn_isFinite = sscglobal_bn_isFinite;
+          let _sscglobal_bn_isInteger = sscglobal_bn_isInteger;
+          let _sscglobal_bn_isPositive = sscglobal_bn_isPositive;
+          let _sscglobal_bn_toFixed = sscglobal_bn_toFixed;
+          let _sscglobal_bn_toNumber = sscglobal_bn_toNumber;
+          let _sscglobal_bn_toString = sscglobal_bn_toString;
+          let _sscglobal_bn_integerValue = sscglobal_bn_integerValue;
+          let _sscglobal_bn_min = sscglobal_bn_min;
+          let _sscglobal_bn_max = sscglobal_bn_max;
+
           let deepUnwrap = (x) => {
             let y = x;
             if (typeof x === "object" && x !== null) {
@@ -622,81 +651,89 @@ class SmartContracts {
           };
           let applyWrapper = (fn) => {
             return async (...args) => {
-              const extArgs = args.map(x => sscglobal_externalCopy(deepUnwrap(x)).copyInto());
+              const extArgs = args.map(x => _sscglobal_externalCopy(deepUnwrap(x)).copyInto());
               const result = await fn.applySyncPromise(undefined, extArgs);
               return (typeof result === 'object' && result.copy) ? await result.copy() : result;
             }
           };
           let applyWrapperSync = (fn) => {
             return (...args) => {
-              const extArgs = args.map(x => sscglobal_externalCopy(deepUnwrap(x)).copyInto());
+              const extArgs = args.map(x => _sscglobal_externalCopy(deepUnwrap(x)).copyInto());
               const result = fn.applySync(undefined, extArgs);
               return (typeof result === 'object' && result.copy) ? result.copy() : result;
             }
           };
           let maybeUnwrap = (x) => {
-            return typeof x === 'object' && x !== null && x.unwrap ? x.unwrap() : x;
+            return typeof x === 'object' && x !== null && x._sscg_unwrap ? _sscglobal_bn_construct.applySync(undefined, [x.toString()]) : x;
           };
           let makeBigNumber = (x) => {
-            return bigNumberWrapper(sscglobal_bn_construct.applySync(undefined, [maybeUnwrap(x)]));
+            return bigNumberWrapper(_sscglobal_bn_construct.applySync(undefined, [maybeUnwrap(x)]));
           };
           let bigNumberWrapper = (x) => {
             return {
-              plus: (y) => bigNumberWrapper(sscglobal_bn_plus.applySync(undefined,[x, maybeUnwrap(y)])),
-              minus: (y) => bigNumberWrapper(sscglobal_bn_minus.applySync(undefined,[x, maybeUnwrap(y)])),
-              times: (y) => bigNumberWrapper(sscglobal_bn_times.applySync(undefined,[x, maybeUnwrap(y)])),
-              multipliedBy: (y, z) => bigNumberWrapper(sscglobal_bn_multipliedBy.applySync(undefined,[x, maybeUnwrap(y), z])),
-              dividedBy: (y, z) => bigNumberWrapper(sscglobal_bn_dividedBy.applySync(undefined,[x, maybeUnwrap(y), z])),
-              sqrt: () => bigNumberWrapper(sscglobal_bn_sqrt.applySync(undefined,[x])),
-              pow: (y) => bigNumberWrapper(sscglobal_bn_pow.applySync(undefined,[x, maybeUnwrap(y)])),
-              negated: () => bigNumberWrapper(sscglobal_bn_negated.applySync(undefined,[x])),
-              abs: () => bigNumberWrapper(sscglobal_bn_abs.applySync(undefined,[x])),
-              lt: (y) => sscglobal_bn_lt.applySync(undefined, [x, maybeUnwrap(y)]),
-              lte: (y) => sscglobal_bn_lte.applySync(undefined, [x, maybeUnwrap(y)]),
-              eq: (y) => sscglobal_bn_eq.applySync(undefined, [x, maybeUnwrap(y)]),
-              gt: (y) => sscglobal_bn_gt.applySync(undefined, [x, maybeUnwrap(y)]),
-              gte: (y) => sscglobal_bn_gte.applySync(undefined, [x, maybeUnwrap(y)]),
-              dp: (y, z) => { const ret = sscglobal_bn_dp.applySync(undefined, [x, y, z]);
+              plus: (y) => bigNumberWrapper(_sscglobal_bn_plus.applySync(undefined,[x, maybeUnwrap(y)])),
+              minus: (y) => bigNumberWrapper(_sscglobal_bn_minus.applySync(undefined,[x, maybeUnwrap(y)])),
+              times: (y) => bigNumberWrapper(_sscglobal_bn_times.applySync(undefined,[x, maybeUnwrap(y)])),
+              multipliedBy: (y, z) => bigNumberWrapper(_sscglobal_bn_multipliedBy.applySync(undefined,[x, maybeUnwrap(y), z])),
+              dividedBy: (y, z) => bigNumberWrapper(_sscglobal_bn_dividedBy.applySync(undefined,[x, maybeUnwrap(y), z])),
+              sqrt: () => bigNumberWrapper(_sscglobal_bn_sqrt.applySync(undefined,[x])),
+              pow: (y) => bigNumberWrapper(_sscglobal_bn_pow.applySync(undefined,[x, maybeUnwrap(y)])),
+              negated: () => bigNumberWrapper(_sscglobal_bn_negated.applySync(undefined,[x])),
+              abs: () => bigNumberWrapper(_sscglobal_bn_abs.applySync(undefined,[x])),
+              lt: (y) => _sscglobal_bn_lt.applySync(undefined, [x, maybeUnwrap(y)]),
+              lte: (y) => _sscglobal_bn_lte.applySync(undefined, [x, maybeUnwrap(y)]),
+              eq: (y) => _sscglobal_bn_eq.applySync(undefined, [x, maybeUnwrap(y)]),
+              gt: (y) => _sscglobal_bn_gt.applySync(undefined, [x, maybeUnwrap(y)]),
+              gte: (y) => _sscglobal_bn_gte.applySync(undefined, [x, maybeUnwrap(y)]),
+              dp: (y, z) => { const ret = _sscglobal_bn_dp.applySync(undefined, [x, y, z]);
                 return typeof y === 'number' ? bigNumberWrapper(ret) : ret;
               },
-              decimalPlaces: (y, z) => { const ret = sscglobal_bn_decimalPlaces.applySync(undefined, [x, y, z]);
+              decimalPlaces: (y, z) => { const ret = _sscglobal_bn_decimalPlaces.applySync(undefined, [x, y, z]);
                 return typeof y === 'number' ? bigNumberWrapper(ret) : ret;
               },
-              isNaN: () => sscglobal_bn_isNaN.applySync(undefined, [x]),
-              isFinite: () => sscglobal_bn_isFinite.applySync(undefined, [x]),
-              isInteger: () => sscglobal_bn_isInteger.applySync(undefined, [x]),
-              isPositive: () => sscglobal_bn_isPositive.applySync(undefined, [x]),
-              toFixed: (y, z) => sscglobal_bn_toFixed.applySync(undefined, [x, y, z]),
-              toNumber: () => sscglobal_bn_toNumber.applySync(undefined, [x]),
-              toString: () => sscglobal_bn_toString.applySync(undefined, [x]),
-              integerValue: (y) => bigNumberWrapper(sscglobal_bn_integerValue.applySync(undefined, [x, y])),
-              unwrap: () => x,
-              [Symbol.toPrimitive]: () => sscglobal_bn_toNumber.applySync(undefined, [x]),
+              isNaN: () => _sscglobal_bn_isNaN.applySync(undefined, [x]),
+              isFinite: () => _sscglobal_bn_isFinite.applySync(undefined, [x]),
+              isInteger: () => _sscglobal_bn_isInteger.applySync(undefined, [x]),
+              isPositive: () => _sscglobal_bn_isPositive.applySync(undefined, [x]),
+              toFixed: (y, z) => _sscglobal_bn_toFixed.applySync(undefined, [x, y, z]),
+              toNumber: () => _sscglobal_bn_toNumber.applySync(undefined, [x]),
+              toString: () => _sscglobal_bn_toString.applySync(undefined, [x]),
+              integerValue: (y) => bigNumberWrapper(_sscglobal_bn_integerValue.applySync(undefined, [x, y])),
+              _sscg_unwrap: true,//() => x,
+              [Symbol.toPrimitive]: () => _sscglobal_bn_toNumber.applySync(undefined, [x]),
             };
           };
+          let getApiProp = (k) => {
+            const v = _sscglobal_api.getSync(k, { copy: true });
+            return typeof v !== 'undefined' ? v : null;
+          };
+          let getDbProp = (k) => {
+            const v = _sscglobal_db.getSync(k, { copy: true });
+            return typeof v !== 'undefined' ? v : null;
+          };
           global.api = {
-            sender: typeof sscglobal_sender !== 'undefined' ? sscglobal_sender : null,
-            owner: typeof sscglobal_owner !== 'undefined' ? sscglobal_owner : null,
-            action: sscglobal_action,
-            payload: typeof sscglobal_payload === 'object' && sscglobal_payload.copy ? sscglobal_payload.copy() : sscglobal_payload,
-            transactionId: sscglobal_transactionId,
-            blockNumber: sscglobal_blockNumber,
-            refHiveBlockNumber: sscglobal_refHiveBlockNumber,
-            hiveBlockTimestamp: sscglobal_hiveBlockTimestamp,
-            contractVersion: sscglobal_contractVersion,
+            sender: getApiProp('sender'),
+            owner: getApiProp('owner'),
+            action: getApiProp('action'),
+            payload: getApiProp('payload'),
+            transactionId: getApiProp('transactionId'),
+            blockNumber: getApiProp('blockNumber'),
+            refHiveBlockNumber: getApiProp('refHiveBlockNumber'),
+            hiveBlockTimestamp: getApiProp('hiveBlockTimestamp'),
+            contractVersion: getApiProp('contractVersion'),
             db: {
-              createTable: typeof sscglobal_createTable !== 'undefined' ? applyWrapper(sscglobal_createTable) : null,
-              addIndexes: typeof sscglobal_addIndexes !== 'undefined' ? applyWrapper(sscglobal_addIndexes) : null,
-              find: applyWrapper(sscglobal_find),
-              findInTable: applyWrapper(sscglobal_findInTable),
-              findOne: applyWrapper(sscglobal_findOne),
-              findOneInTable: applyWrapper(sscglobal_findOneInTable),
-              findContract: applyWrapper(sscglobal_findContract),
-              insert: applyWrapper(sscglobal_insert),
-              remove: applyWrapper(sscglobal_remove),
-              update: applyWrapper(sscglobal_update),
-              tableExists: applyWrapper(sscglobal_tableExists),
-              getBlockInfo: typeof sscglobal_getBlockInfo !== 'undefined' ? applyWrapper(sscglobal_getBlockInfo) : null,
+              createTable: applyWrapper(getDbProp('createTable')),
+              addIndexes: applyWrapper(getDbProp('addIndexes')),
+              find: applyWrapper(getDbProp('find')),
+              findInTable: applyWrapper(getDbProp('findInTable')),
+              findOne: applyWrapper(getDbProp('findOne')),
+              findOneInTable: applyWrapper(getDbProp('findOneInTable')),
+              findContract: applyWrapper(getDbProp('findContract')),
+              insert: applyWrapper(getDbProp('insert')),
+              remove: applyWrapper(getDbProp('remove')),
+              update: applyWrapper(getDbProp('update')),
+              tableExists: applyWrapper(getDbProp('tableExists')),
+              getBlockInfo: applyWrapper(getDbProp('getBlockInfo')),
             },
             BigNumber: makeBigNumber,
             validator: {
@@ -707,19 +744,19 @@ class SmartContracts {
               isIP: sscglobalv_isIP,
               isFQDN: sscglobalv_isFQDN,
             },
-            logs: typeof sscglobal_logs !== 'undefined' ? applyWrapperSync(sscglobal_logs) : null,
-            SHA256: applyWrapperSync(sscglobal_SHA256),
-            checkSignature: applyWrapperSync(sscglobal_checkSignature),
-            random: applyWrapperSync(sscglobal_random),
-            debug: sscglobal_debug,
-            executeSmartContract: applyWrapper(sscglobal_executeSmartContract),
-            executeSmartContractAsOwner: typeof sscglobal_executeSmartContractAsOwner !== 'undefined' ? applyWrapper(sscglobal_executeSmartContractAsOwner) : null,
-            transferTokens: typeof sscglobal_transferTokens !== 'undefined' ? applyWrapper(sscglobal_transferTokens) : null,
-            transferTokensFromCallingContract: typeof sscglobal_transferTokensFromCallingContract !== 'undefined' ? applyWrapper(sscglobal_transferTokensFromCallingContract) : null,
-            verifyBlock: typeof sscglobal_verifyBlock !== 'undefined' ? applyWrapper(sscglobal_verifyBlock) : null,
-            emit: applyWrapper(sscglobal_emit),
-            assert: (condition, message) => sscglobal_assert.applySync(undefined, [!!condition, message]).copy(),
-            isValidAccountName: applyWrapperSync(sscglobal_isValidAccountName),
+            logs: applyWrapperSync(getApiProp('logs')),
+            SHA256: applyWrapperSync(getApiProp('SHA256')),
+            checkSignature: applyWrapperSync(getApiProp('checkSignature')),
+            random: applyWrapperSync(getApiProp('random')),
+            debug: _sscglobal_debug,
+            executeSmartContract: applyWrapper(getApiProp('executeSmartContract')),
+            executeSmartContractAsOwner: applyWrapper(getApiProp('executeSmartContractAsOwner')),
+            transferTokens: applyWrapper(getApiProp('transferTokens')),
+            transferTokensFromCallingContract: applyWrapper(getApiProp('transferTokensFromCallingContract')),
+            verifyBlock: applyWrapper(getApiProp('verifyBlock')),
+            emit: applyWrapper(getApiProp('emit')),
+            assert: (condition, message) => getApiProp('assert').applySync(undefined, [!!condition, message]).copy(),
+            isValidAccountName: applyWrapperSync(getApiProp('isValidAccountName')),
           };
           global.api.BigNumber.ROUND_UP = 0;
           global.api.BigNumber.ROUND_DOWN = 1;
@@ -730,21 +767,47 @@ class SmartContracts {
           global.api.BigNumber.ROUND_HALF_EVEN = 6;
           global.api.BigNumber.ROUND_HALF_CEIL = 7;
           global.api.BigNumber.ROUND_HALF_FLOOR = 8;
-          global.api.BigNumber.min = (...args) => bigNumberWrapper(sscglobal_bn_min.applySync(undefined, args.map(maybeUnwrap)));
-          global.api.BigNumber.max = (...args) => bigNumberWrapper(sscglobal_bn_max.applySync(undefined, args.map(maybeUnwrap)));
+          global.api.BigNumber.min = (...args) => bigNumberWrapper(_sscglobal_bn_min.applySync(undefined, args.map(maybeUnwrap)));
+          global.api.BigNumber.max = (...args) => bigNumberWrapper(_sscglobal_bn_max.applySync(undefined, args.map(maybeUnwrap)));
         };
+
         const compiledWrapper = vm.isolate.compileScriptSync(wrappedCode);
         const compiled = vm.isolate.compileScriptSync(contractCode);
-        //vm.isolate.startCpuProfiler("profssc");
         compiledWrapper.run(vm.context).then(() => {
-          /*vm.isolate.stopCpuProfiler("profssc").then((tcpArr) => {
-            tcpArr.forEach(x => {
-              if (x.profile.endTime - x.profile.startTime > 500000) { console.log(JSON.stringify(x)); }
-            });*/
+            vm.context.global.delete('sscglobal_api');
+            vm.context.global.delete('sscglobal_debug');
+            vm.context.global.delete('sscglobal_db');
+            vm.context.global.delete('sscglobal_externalCopy');
+            vm.context.global.delete('sscglobal_bn_construct');
+            vm.context.global.delete('sscglobal_bn_plus');
+            vm.context.global.delete('sscglobal_bn_minus');
+            vm.context.global.delete('sscglobal_bn_times');
+            vm.context.global.delete('sscglobal_bn_multipliedBy');
+            vm.context.global.delete('sscglobal_bn_dividedBy');
+            vm.context.global.delete('sscglobal_bn_sqrt');
+            vm.context.global.delete('sscglobal_bn_pow');
+            vm.context.global.delete('sscglobal_bn_negated');
+            vm.context.global.delete('sscglobal_bn_abs');
+            vm.context.global.delete('sscglobal_bn_lt');
+            vm.context.global.delete('sscglobal_bn_lte');
+            vm.context.global.delete('sscglobal_bn_eq');
+            vm.context.global.delete('sscglobal_bn_gt');
+            vm.context.global.delete('sscglobal_bn_gte');
+            vm.context.global.delete('sscglobal_bn_dp');
+            vm.context.global.delete('sscglobal_bn_decimalPlaces');
+            vm.context.global.delete('sscglobal_bn_isNaN');
+            vm.context.global.delete('sscglobal_bn_isFinite');
+            vm.context.global.delete('sscglobal_bn_isInteger');
+            vm.context.global.delete('sscglobal_bn_isPositive');
+            vm.context.global.delete('sscglobal_bn_toFixed');
+            vm.context.global.delete('sscglobal_bn_toNumber');
+            vm.context.global.delete('sscglobal_bn_toString');
+            vm.context.global.delete('sscglobal_bn_integerValue');
+            vm.context.global.delete('sscglobal_bn_min');
+            vm.context.global.delete('sscglobal_bn_max');
             compiled.run(vm.context).then(() => {
               vm.isolate.dispose();
             });
-          //});
         });
       } else {
         resolve('no JS VM available');
