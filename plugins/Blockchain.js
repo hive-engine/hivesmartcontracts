116a523d8171ad0e31938ab351fa581a84fadb20
eonwarped <eonwarped@gmail.com>
2025-02-06 09:08:19 +0100

    restore branch state to hive/qa with the tx limit fixes

--- plugins/Blockchain.js
+++ plugins/Blockchain.js
@@ -17,6 +17,7 @@ let javascriptVMTimeout = 0;
 let producing = false;
 let stopRequested = false;
 let hashVerificationNode = false;
+let enablePerUserTxLimit = true;
 
 const createGenesisBlock = async (payload) => {
   // check if genesis block hasn't been generated already
@@ -101,6 +102,7 @@ async function producePendingTransactions(
       previousBlock.blockNumber,
       previousBlock.hash,
       previousBlock.databaseHash,
+      enablePerUserTxLimit
     );
     log.info(`Start session for block ${newBlock.blockNumber}`);
     const session = database.startSession();
@@ -198,6 +200,7 @@ const init = async (conf, callback) => {
   } = conf;
   javascriptVMTimeout = conf.javascriptVMTimeout; // eslint-disable-line prefer-destructuring
   hashVerificationNode = conf.hashVerificationNode; // eslint-disable-line prefer-destructuring
+  enablePerUserTxLimit = conf.enablePerUserTxLimit; // eslint-disable-line prefer-destructuring
   log.setDefaultLevel(conf.defaultLogLevel ? conf.defaultLogLevel : 'warn');
 
   database = new Database();
