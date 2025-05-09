0952794fdd6cfafc44202329372a9196e244f2e1
BostonTechie <andrewurq@gmail.com>
2025-05-09 11:36:38 -0400

    backwards

--- test/stopspam.js
+++ test/stopspam.js
@@ -80,7 +80,7 @@ describe('stopspam', function () {
       transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'stopspam', 'updateParams', '{ "numberOfFreeTx": "4"}'));
       transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'stopspam', 'addAccount', '{ "allowList": ["drew","tim"] , "denyList": ["god0","god1"] }'));
       transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'stopspam', 'addAccount', '{ "allowList": ["gus","todd"] , "denyList": ["god2","god3"] }'))
-      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'stopspam', 'removeAccount', '{ "allowList": ["gus"] , "denyList": ["god2"] }'))
+
       let block = {
         refHiveBlockNumber: refBlockNumber,
         refHiveBlockId: 'ABCD1',
@@ -104,7 +104,7 @@ describe('stopspam', function () {
       });
       console.log(" ")
       console.log( '\u001b[' + 93 + 'm' + 'Test: update params on stopspam.js' + '\u001b[0m')
-      console.log("26  ⚪",JSON.parse(transactionsBlock1[6].logs))
+      console.log("26  ⚪",JSON.parse(transactionsBlock1[4].logs))
       console.log(params);
 
 
@@ -116,59 +116,59 @@ describe('stopspam', function () {
       });
   });
 
-  // it('it counts transactions', (done) => {
-  //   new Promise(async (resolve) => {
+  it('it counts transactions', (done) => {
+    new Promise(async (resolve) => {
 
-  //     await fixture.setUp();
+      await fixture.setUp();
 
-  //     let refBlockNumber = fixture.getNextRefBlockNumber();
-  //     let transactions = [];
-  //     transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'contract', 'update', JSON.stringify(tknContractPayload)));
-  //     transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'contract', 'deploy', JSON.stringify(ContractPayload)));
-  //     transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'contract', 'update', JSON.stringify(ContractPayload)));
-  //     transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'tokens', 'transfer', `{ "symbol": "${CONSTANTS.UTILITY_TOKEN_SYMBOL}", "to": "harpagon", "quantity": "100000", "isSignedWithActiveKey": true }`));
-  //     transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), 'harpagon', 'tokens', 'transfer', '{ "symbol": "BEE", "quantity": "1", "to": "drewlongshot", "isSignedWithActiveKey": true }'));
-  //     transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), 'harpagon', 'tokens', 'transfer', '{ "symbol": "BEE", "quantity": "2", "to": "drewlongshot", "isSignedWithActiveKey": true }'));
-  //     transactions.push(new Transaction(refBlockNumber+1, fixture.getNextTxId(), 'harpagon', 'tokens', 'transfer', '{ "symbol": "BEE", "quantity": "3", "to": "drewlongshot", "isSignedWithActiveKey": true }'));
+      let refBlockNumber = fixture.getNextRefBlockNumber();
+      let transactions = [];
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'contract', 'update', JSON.stringify(tknContractPayload)));
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'contract', 'deploy', JSON.stringify(ContractPayload)));
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'contract', 'update', JSON.stringify(ContractPayload)));
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'tokens', 'transfer', `{ "symbol": "${CONSTANTS.UTILITY_TOKEN_SYMBOL}", "to": "harpagon", "quantity": "100000", "isSignedWithActiveKey": true }`));
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), 'harpagon', 'tokens', 'transfer', '{ "symbol": "BEE", "quantity": "1", "to": "drewlongshot", "isSignedWithActiveKey": true }'));
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), 'harpagon', 'tokens', 'transfer', '{ "symbol": "BEE", "quantity": "2", "to": "drewlongshot", "isSignedWithActiveKey": true }'));
+      transactions.push(new Transaction(refBlockNumber+1, fixture.getNextTxId(), 'harpagon', 'tokens', 'transfer', '{ "symbol": "BEE", "quantity": "3", "to": "drewlongshot", "isSignedWithActiveKey": true }'));
 
-  //  let block = {
-  //       refHiveBlockNumber: refBlockNumber,
-  //       refHiveBlockId: 'ABCD1',
-  //       prevRefHiveBlockId: 'ABCD2',
-  //       timestamp: '2018-06-01T00:00:00',
-  //       transactions,
-  //     };
+   let block = {
+        refHiveBlockNumber: refBlockNumber,
+        refHiveBlockId: 'ABCD1',
+        prevRefHiveBlockId: 'ABCD2',
+        timestamp: '2018-06-01T00:00:00',
+        transactions,
+      };
 
-  //     await fixture.sendBlock(block);
+      await fixture.sendBlock(block);
 
-  //     const res = await fixture.database.getBlockInfo(1);
+      const res = await fixture.database.getBlockInfo(1);
 
-  //     const block1 = res;
-  //     const transactionsBlock1 = block1.transactions;
+      const block1 = res;
+      const transactionsBlock1 = block1.transactions;
 
-  //          let res2 = await fixture.database.findOne({
-  //             contract: 'tokens',
-  //             table: 'balances',
-  //             query: {account:'harpagon',}
-  //           });
+           let res2 = await fixture.database.findOne({
+              contract: 'tokens',
+              table: 'balances',
+              query: {account:'harpagon',}
+            });
       
-  //           let token = res2
+            let token = res2
       
 
-  //     console.log(" ")
-  //     console.log( '\u001b[' + 93 + 'm' + 'Test: counts transactions by user' + '\u001b[0m')
-  //     // console.log("  ⚪ ",JSON.parse(transactionsBlock1[4].logs))
-  //     // console.log("  ⚪ ",JSON.parse(transactionsBlock1[5].logs))
-  //     // console.log("  ⚪ ",JSON.parse(transactionsBlock1[6].logs))
+      console.log(" ")
+      console.log( '\u001b[' + 93 + 'm' + 'Test: counts transactions by user' + '\u001b[0m')
+      // console.log("  ⚪ ",JSON.parse(transactionsBlock1[4].logs))
+      // console.log("  ⚪ ",JSON.parse(transactionsBlock1[5].logs))
+      // console.log("  ⚪ ",JSON.parse(transactionsBlock1[6].logs))
 
 
-  //     resolve();
-  //   })
-  //     .then(() => {
-  //       fixture.tearDown();
-  //       done();
-  //     });
-  // });
+      resolve();
+    })
+      .then(() => {
+        fixture.tearDown();
+        done();
+      });
+  });
 
   // it('does not update parameters', (done) => {
   //   new Promise(async (resolve) => {
