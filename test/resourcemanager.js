1bc75434e8852a9e1a7b2be4623f6c19c2cfece6
Endecs <info@endecs.com>
2025-05-10 21:30:00 +0300

    started with testing code

--- test/resourcemanager.js
+++ test/resourcemanager.js
@@ -11,17 +11,15 @@ const { Fixture, conf } = require('../libs/util/testing/Fixture');
 const { TableAsserts } = require('../libs/util/testing/TableAsserts');
 const { assertError } = require('../libs/util/testing/Asserts');
 
-
 const tknContractPayload = setupContractPayload('tokens', './contracts/tokens.js');
 const ContractPayload = setupContractPayload('resourcemanager', './contracts/resourcemanager.js');
-const mpContractPayload = setupContractPayload('marketpools', './contracts/marketpools.js');
 
 const fixture = new Fixture();
 const tableAsserts = new TableAsserts(fixture);
 
 // test cases for resourcemanager smart contract
 describe('resourcemanager', function () {
-  this.timeout(4000);
+  this.timeout(1000000);
 
   before((done) => {
     new Promise(async (resolve) => {
@@ -67,60 +65,56 @@ describe('resourcemanager', function () {
       })
   });
 
-  it('how does block.js function?', (done) => {
+  async function initializeResourceManager() {
+     // Initialize new resourcemanager contract before starting block 95935754 
+     let refBlockNumber = 95935753;
+     let transactions = [];
+     // deploy contracts
+     transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'contract', 'update', JSON.stringify(tknContractPayload)));
+     transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'contract', 'deploy', JSON.stringify(ContractPayload)));
+     transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'contract', 'update', JSON.stringify(ContractPayload)));
+     
+     // Create BEED token if not already available
+     transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'tokens', 'create', '{ "isSignedWithActiveKey": true,  "name": "token", "url": "https://token.com", "symbol": "BEED", "precision": 8, "maxSupply": "1000" }'));
+     // Issue some tokens to drew
+     transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'tokens', 'issue', '{ "symbol": "BEED", "to": "drew", "quantity": "1", "isSignedWithActiveKey": true }'));
+
+     let block = {
+       refHiveBlockNumber: refBlockNumber,
+       refHiveBlockId: 'ABCD1',
+       prevRefHiveBlockId: 'ABCD2',
+       timestamp: '2018-06-01T00:00:00',
+       transactions,
+     };
+     
+     // process all transactions defined above in block
+     await fixture.sendBlock(block);
+  }
+
+  it('one action is free', (done) => {
     new Promise(async (resolve) => {
 
       await fixture.setUp();
 
-      let refBlockNumber = fixture.getNextRefBlockNumber();
-      let transactions = [];
-      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'contract', 'update', JSON.stringify(tknContractPayload)));
-      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'contract', 'deploy', JSON.stringify(ContractPayload)));
-      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'contract', 'update', JSON.stringify(ContractPayload)));
-      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'tokens', 'transfer', `{ "symbol": "${CONSTANTS.UTILITY_TOKEN_SYMBOL}", "to": "harpagon", "quantity": "100000", "isSignedWithActiveKey": true }`));
-      transactions.push(new Transaction(refBlockNumber+1, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'resourcemanager', 'updateParams', '{ "numberOfFreeTx": "4"}'));
-      transactions.push(new Transaction(refBlockNumber+1, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'resourcemanager', 'addAccount', '{ "allowList": ["drew","tim"] , "denyList": ["hate","hate1"] }'));
-      transactions.push(new Transaction(refBlockNumber+1, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'resourcemanager', 'addAccount', '{ "allowList": ["gus","todd"] , "denyList": ["hate2","hate3"] }'))
-      transactions.push(new Transaction(refBlockNumber+2, fixture.getNextTxId(), 'harpagon', 'tokens', 'transfer', '{ "symbol": "BEE", "quantity": "1", "to": "drewlongshot", "isSignedWithActiveKey": true }'));
-      transactions.push(new Transaction(refBlockNumber+2, fixture.getNextTxId(), 'harpagon', 'tokens', 'transfer', '{ "symbol": "BEE", "quantity": "2", "to": "drewlongshot", "isSignedWithActiveKey": true }'));
-      transactions.push(new Transaction(refBlockNumber+2, fixture.getNextTxId(), 'harpagon', 'tokens', 'transfer', '{ "symbol": "BEE", "quantity": "3", "to": "drewlongshot", "isSignedWithActiveKey": true }'));
-      
+      await initializeResourceManager();
 
-   let block = {
+      // one transaction should be free
+      let refBlockNumber = 95935754;
+      transactions = [];
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), 'drew', 'tokens', 'transfer', '{ "symbol": "BEED", "quantity": "1", "to": "drewlongshot", "isSignedWithActiveKey": true }'));
+      
+      let block = {
         refHiveBlockNumber: refBlockNumber,
         refHiveBlockId: 'ABCD1',
         prevRefHiveBlockId: 'ABCD2',
         timestamp: '2018-06-01T00:00:00',
         transactions,
       };
-
       await fixture.sendBlock(block);
 
-      const res = await fixture.database.getBlockInfo(1);
+      const res = await fixture.database.getBlockInfo(2);
 
-      const block1 = res;
-      const transactionsBlock1 = block1.transactions;
-      const params = await fixture.database.findOne({
-        contract: 'resourcemanager',
-        table: 'params',
-        query: {}
-      });
-      
-            let table = params
-      
-
-      console.log(" ")
-      console.log( '\u001b[' + 93 + 'm' + 'Test: how does block.js function' + '\u001b[0m')
-      console.log(table)
-
-      // console.log(transactions[4])
-      // console.log(transactions[5])
-      // console.log(transactions[6])
-
-      // console.log("  ⚪ ",JSON.parse(transactionsBlock1[4].logs))
-      // console.log("  ⚪ ",JSON.parse(transactionsBlock1[5].logs))
-      // console.log("  ⚪ ",JSON.parse(transactionsBlock1[6].logs))
-
+      //TODO: Check if transfer was successfully
 
      resolve();
     })
@@ -130,4 +124,38 @@ describe('resourcemanager', function () {
       });
   });
 
+  it('two actions', (done) => {
+    new Promise(async (resolve) => {
+
+      await fixture.setUp();
+
+      await initializeResourceManager();
+
+      // one transaction should be free
+      let refBlockNumber = 95935754;
+      transactions = [];
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), 'drew', 'tokens', 'transfer', '{ "symbol": "BEED", "quantity": "0.001", "to": "drewlongshot", "isSignedWithActiveKey": true }'));
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), 'drew', 'tokens', 'transfer', '{ "symbol": "BEED", "quantity": "0.001", "to": "drewlongshot", "isSignedWithActiveKey": true }'));
+      
+      let block = {
+        refHiveBlockNumber: refBlockNumber,
+        refHiveBlockId: 'ABCD1',
+        prevRefHiveBlockId: 'ABCD2',
+        timestamp: '2018-06-01T00:00:00',
+        transactions,
+      };
+      await fixture.sendBlock(block);
+
+      const res = await fixture.database.getBlockInfo(2);
+
+      //TODO: Check if transfer was successfully
+      //TODO: Check if drew balance has now 0.997 BEED (0.002 sent away + 0.001 burned)
+
+     resolve();
+    })
+      .then(() => {
+        fixture.tearDown();
+        done();
+      });
+  });
 });
