d54afc2ffc5778a57bbbc6140b2ddf4bbd594309
Endecs <info@endecs.com>
2025-05-12 19:11:24 +0300

    added denyList 24h check

--- test/resourcemanager.js
+++ test/resourcemanager.js
@@ -159,7 +159,7 @@ describe('resourcemanager', function () {
       });
   });
 
-  it('add to denyList', (done) => {
+  it('add to denyList and get blocked', (done) => {
     new Promise(async (resolve) => {
 
       await fixture.setUp();
@@ -195,4 +195,57 @@ describe('resourcemanager', function () {
       });
   });
   
+  it('denyList 24h check', (done) => {
+    new Promise(async (resolve) => {
+
+      await fixture.setUp();
+
+      await initializeResourceManager();
+
+      // one transaction should be free
+      let refBlockNumber = 95935754;
+      transactions = [];
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), CONSTANTS.HIVE_ENGINE_ACCOUNT, 'resourcemanager', 'addAccount', '{"denyList": ["drew", "satoshi"]}' ));
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), 'drew', 'tokens', 'transfer', '{ "symbol": "BEED", "quantity": "0.001", "to": "drewlongshot", "isSignedWithActiveKey": true }'));
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), 'drew', 'tokens', 'transfer', '{ "symbol": "BEED", "quantity": "0.001", "to": "drewlongshot", "isSignedWithActiveKey": true }'));
+      
+      let block = {
+        refHiveBlockNumber: refBlockNumber,
+        refHiveBlockId: 'ABCD1',
+        prevRefHiveBlockId: 'ABCD2',
+        timestamp: '2025-05-12T16:30:03',
+        transactions,
+      };
+      await fixture.sendBlock(block);
+
+      let res = await fixture.database.getBlockInfo(2);
+
+      //TODO: Check if one transfer was successfully
+      //TODO: Second transaction has to be blocked
+      refBlockNumber = 95935755;
+      transactions = [];
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), 'drew', 'tokens', 'transfer', '{ "symbol": "BEED", "quantity": "0.001", "to": "drewlongshot", "isSignedWithActiveKey": true }'));
+      transactions.push(new Transaction(refBlockNumber, fixture.getNextTxId(), 'drew', 'tokens', 'transfer', '{ "symbol": "BEED", "quantity": "0.001", "to": "drewlongshot", "isSignedWithActiveKey": true }'));
+      
+      block = {
+        refHiveBlockNumber: refBlockNumber,
+        refHiveBlockId: 'ABCD1',
+        prevRefHiveBlockId: 'ABCD2',
+        timestamp: '2025-05-13T16:30:03',
+        transactions,
+      };
+      await fixture.sendBlock(block);
+
+      res = await fixture.database.getBlockInfo(3);
+
+      // TODO: first tx should go through as 24h are over.
+      //TODO: Second transaction has to be blocked
+
+     resolve();
+    })
+      .then(() => {
+        fixture.tearDown();
+        done();
+      });
+  });
 });
