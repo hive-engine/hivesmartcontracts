4964a490ba4e3b5414915622715b0a9b04dca926
Endecs <info@endecs.com>
2025-05-13 20:29:54 +0300

    PR adjustments

--- contracts/resourcemanager.js
+++ contracts/resourcemanager.js
@@ -30,35 +30,45 @@ actions.createSSC = async () => {
 };
 
 actions.updateParams = async (payload) => {
-  if (api.sender !== api.owner) return;
+  if (!api.assert(sender === api.owner, 'not authorized')) 
+    return;
 
   const {
     numberOfFreeTx,
     multiTransactionFee,
     burnSymbol,
+    denyMaxTx
   } = payload;
 
   const params = await api.db.findOne('params', {});
 
-  if (numberOfFreeTx && Number.isInteger(numberOfFreeTx) && numberOfFreeTx >= 1) {
+  if (numberOfFreeTx){
+    if (!api.assert(Number.isInteger(numberOfFreeTx) && numberOfFreeTx >= 1), 'invalid numberOfFreeTx') 
+      return;
     params.numberOfFreeTx = numberOfFreeTx;
   }
 
-  const feeBN = api.BigNumber(multiTransactionFee);
-  if (multiTransactionFee && typeof multiTransactionFee === 'string' && !feeBN.isNaN() && feeBN.gte(0) && !feeBN.isFinite()) {
+  if (multiTransactionFee){
+    const feeBN = api.BigNumber(multiTransactionFee);
+    if (!api.assert(typeof multiTransactionFee === 'string' && !feeBN.isNaN() && feeBN.gte(0) && feeBN.isFinite()), 'invalid multiTransactionFee') 
+      return;
     params.multiTransactionFee = multiTransactionFee;
   }
-
-  if (denyMaxTx && Number.isInteger(denyMaxTx) && denyMaxTx >= 0) {
+  
+  if (denyMaxTx){
+    if (!api.assert(Number.isInteger(denyMaxTx) && denyMaxTx >= 0), 'invalid denyMaxTx') 
+      return;
     params.denyMaxTx = denyMaxTx;
   }
 
-  if (burnSymbol && typeof burnSymbol === 'string') {
+  if (burnSymbol) {
+    if (!api.assert(typeof burnSymbol === 'string'), 'invalid burnSymbol') 
+      return;
     // check if the token exists
-    const token = await api.db.findOne('tokens', { symbol });
-    if (token) {
-      params.burnSymbol = burnSymbol;
-    }
+    const token = await api.db.findOne('tokens', { symbol: burnSymbol });
+    if (!api.assert(token), 'burnSymbol not available') 
+      return;
+    params.burnSymbol = burnSymbol;
   }
 
   await api.db.update('params', params);
@@ -68,54 +78,82 @@ actions.updateAccount = async (payload) => {
   const sender = api.sender;
 
   const moderator = await api.db.findOne('moderators', {account: sender});
-
-  if (sender !== api.owner && !moderator) 
+  if (!api.assert(sender === api.owner || moderator, 'not authorized')) 
     return;
 
   const table = 'accountControls';
   const { account, isDenied, isAllowed } = payload;
   const timestamp = new Date(`${api.hiveBlockTimestamp}.000Z`).getTime();
+
   const accountControl = await api.db.findOne(table, { account });
   const create = !accountControl;
 
-  const updateAccountControl = setAccountControlProperties(accountControl, account, isDenied, isAllowed, timestamp);
+  if (!accountControl)
+    accountControl = { account };
+
+  if (!api.assert(accountControl.account != null && accountControl.account !== 'undefined', 'invalid account')) 
+    return;
+
+  if (isDenied != null) {
+    if (!api.assert(typeof isDenied === 'boolean', 'invalid isDenied')) 
+      return;
+    accountControl.isDenied = isDenied;
+  }
+
+  if (isAllowed != null) {
+    if (!api.assert(typeof isAllowed === 'boolean', 'invalid isDenied')) 
+      return;
+    accountControl.isAllowed = isAllowed;
+  }
+
+  accountControl.lastAction = timestamp;
 
   if (create)
     await api.db.insert(table, updateAccountControl);
   else
     await api.db.update(table, updateAccountControl);
 
-    api.emit('updateAccount', {
+  api.emit('updateAccount', {
       from: api.sender, updatedAccount: account, isDenied, isAllowed
     });
 };
 
 actions.updateModerator = async (payload) => {
   const sender = api.sender;
-  if (sender !== api.owner) 
+  if (!api.assert(sender === api.owner, 'not authorized')) 
     return;
 
   const table = 'moderators';
   const { account, action } = payload;
 
   const moderator = await api.db.findOne(table, {account});
-  
-  if (action === 'add' && !moderator)
-    await api.db.insert(table, {account});
-  
-  if (action === 'remove' && moderator)
-    await api.db.remove(table, moderator);
+  let updated = false;
 
-  api.emit('updateModerator', {
-      from: api.sender, account, action
+  if (action === 'add' && !moderator){
+    await api.db.insert(table, {account});
+    updated = true;
+  }
+  
+  if (action === 'remove' && moderator){
+    await api.db.remove(table, moderator);
+    updated = true;
+  }
+
+  if (updated){
+    api.emit('updateModerator', {
+        from: api.sender, account, action
     });
+  }
 };
 
 actions.burnFee = async (payload) => {
   const sender = api.sender;
   const burnParams = await api.db.findOne('params', {});
   const accountControls = await api.db.findOne('accountControls', { account: sender });
-  
+
+  if (sender === 'null' || sender == null)
+    return;
+
   if (accountControls && accountControls.isDenied){
     // check first if account is denied before
     const nowTimestamp = new Date(`${api.hiveBlockTimestamp}.000Z`).getTime();
@@ -165,20 +203,3 @@ const transferIsSuccessful = (result, action, from, to, symbol, quantity) => res
     && el.data.to === to
     && api.BigNumber(el.data.quantity).eq(quantity)
     && el.data.symbol === symbol) !== undefined;
-
-function setAccountControlProperties(accountControl, account, isDenied, isAllowed, timestamp)
-{
-  if (!accountControl)
-    accountControl = {};
-
-  if (accountControl.account == null || accountControl.account === 'undefined')
-    accountControl.account = account;
-  if (isDenied != null && isDenied !== 'undefined')
-    accountControl.isDenied = isDenied;
-  if (isAllowed != null && isAllowed !== 'undefined')
-    accountControl.isAllowed = isAllowed;
-
-  accountControl.lastAction = timestamp;
-
-  return accountControl;
-}
